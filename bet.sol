b33f9ba847c54ca6a3283061ef5ccc6812d664be
eneumann94 <43457445+eneumann94@users.noreply.github.com>
2018-11-26 16:58:44 -0200

    Update bet.sol

--- bet.sol
+++ bet.sol
@@ -3,19 +3,25 @@ pragma solidity 0.4.25;
 contract Aposta {
 	
     string public nomeAposta;
+    string public descricaoAposta;
+    uint public valorAposta;
+    string public vencedorAposta;
     address apostador1;
     address apostador2;
     address mediador;
-	
-    modifier somenteApostadores() {
-        require(msg.sender==apostador1, msg.sender==apostador2, "Somente apostadores podem realizar essa operação");
-        _;
+    
+    modifier somenteApostador1() {
+        require(msg.sender==apostador1, "Somente o Apostador1 pode realizar essa operação");
     }
     
+    modifier somenteApostador2() {
+        require(msg.sender==apostador2, "Somente o Apostador2 pode realizar essa operação");
+    }
+	
     modifier somenteMediador() {
         require(msg.sender==mediador, "Somente o mediador pode realizar essa operação");
     }
-
+    
     constructor() public {
         nomeAposta = "Libertadores RiverxBoca";
         descricaoAposta = "Quem vai ganhar no jogo RiverXBoca na final da Libertadores";
@@ -32,9 +38,25 @@ contract Aposta {
 	    require(qualApostador2 != address(0), "Endereço de apostador invalido");
 	}
 	
-    function definirMediador(address qualMediador) public somenteApostadores  {
-        require(qualMediador != address(0), "Endereço de mediador invalido");
-        mediador = qualMediador;
+    function definirMediador1(address qualMediador1) public somenteApostador1  {
+        require(qualMediador1 != address(0), "Endereço de mediador invalido");
+        mediador = qualMediador1;
+    }
+    
+    function definirMediador2(address qualMediador2) public somenteApostador2  {
+        require(qualMediador2 != address(0), "Endereço de mediador invalido");
+        mediador = qualMediador2;
+    }
+    
+    if (qualMediador1 = qualMediador2) {
+        mediador = qualMediador1
+    } else {
+        return "Escolha novamente o mediador"
+    }
+    
+    function definirMediador2(address qualMediador1) public somenteApostador2  {
+        require(qualMediador2 != address(0), "Endereço de mediador invalido");
+        mediador = qualMediado2;
     }
 	
     function definirNomeDaAposta(string qualNomeDaAposta) public somenteMediador  {
